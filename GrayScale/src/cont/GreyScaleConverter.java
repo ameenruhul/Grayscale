23f3b2242ac0a7d155062105626ac81a60532f87
Ameen Ruhul Mohammad <ameensunny242@gmail.com>
2017-11-19 21:07:45 +0600

    Library apachecsv.jar needed

---
+++ GrayScale/src/cont/GreyScaleConverter.java
@@ -0,0 +1,40 @@
+package cont;
+
+import java.io.*;
+import edu.duke.*;
+
+
+public class GreyScaleConverter {
+
+    public ImageResource makeGray(ImageResource inImage){
+        ImageResource outImage = new ImageResource(inImage.getWidth(), inImage.getHeight());
+        for(Pixel pixel: outImage.pixels()){
+            Pixel inPixel = inImage.getPixel(pixel.getX(), pixel.getY());
+            int average = (inPixel.getRed() + inPixel.getBlue() + inPixel.getGreen())/3;
+            pixel.setRed(average);
+            pixel.setGreen(average);
+            pixel.setBlue(average);
+        }
+        return outImage;
+    }
+
+    public void testGray(){
+        ImageResource ir = new ImageResource();
+        ImageResource gray = makeGray(ir);
+        gray.draw();
+    }
+
+   public void selectAndConvert(){
+
+            DirectoryResource dr = new DirectoryResource();
+            for(File f: dr.selectedFiles()){
+                ImageResource inImage = new ImageResource(f);
+                ImageResource gray = makeGray(inImage);
+                gray.draw();
+
+
+
+
+        }
+    }
+}
---
+++ GrayScale/src/cont/test.java
@@ -0,0 +1,11 @@
+package cont;
+
+public class test {
+
+    public static void main(String [] args )
+    {
+        GreyScaleConverter test = new GreyScaleConverter();
+        test.selectAndConvert();
+
+    }
+}
